50e1cd1e89786c674bb8af5ba9b8c70c0d008a0d
DESKTOP-NPLM7LE\cube <sypark>
2023-03-16 17:55:12 +0900

    뷰 베이직

--- section01_vueStart/gs-01-starting-project/app.js
+++ section01_vueStart/gs-01-starting-project/app.js
@@ -1,23 +1,21 @@
 Vue.createApp({
-    data() {
-        return{
-            goals: [],
-            enteredValue: ''
-        };
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
     },
-    methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue)
-            this.enteredValue = ''
-        }
-    }
-}).mount('#app');
+  },
+}).mount("#app");
 
 // const buttonEl = document.querySelector('button')
 // const inputEl =  document.querySelector('input')
 // const listEl =  document.querySelector('ul')
-
-
 
 // function addGoal() {
 //     const inputVal = inputEl.value
@@ -27,4 +25,3 @@ Vue.createApp({
 //     inputEl.value= ''
 // }
 // buttonEl.addEventListener('click', addGoal)
-
