f1237e0547724cb882ddc87e72b1e8b30b19f09b
DESKTOP-NPLM7LE\cube <sypark>
2023-03-17 18:00:25 +0900

    기초공부

--- section02_vueBasic/basics-01-starting-code/app.js
+++ section02_vueBasic/basics-01-starting-code/app.js
@@ -1,8 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn Vue'
+            courseGoalA: 'Finish the course and learn Vue',
+            courseGoalB: 'Master learn Vue',
+            vueLink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomeNumber = Math.random()
+            if (randomeNumber < 0.5) {
+                return this.courseGoalA
+            } else {
+                return this.courseGoalB
+            }
+
+        }
     } 
 });
 
