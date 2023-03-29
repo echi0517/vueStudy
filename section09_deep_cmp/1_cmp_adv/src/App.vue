c757eeb48523a396e2b04ff3807b387f8ad8601f
DESKTOP-4RS8SOV\sypark <echi0517@naver.com>
2023-03-30 06:09:49 +0900

    slot 공부

--- section09_deep_cmp/1_cmp_adv/src/App.vue
+++ section09_deep_cmp/1_cmp_adv/src/App.vue
@@ -8,26 +8,34 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+
+        <h2> {{ slotProps.item }}</h2>
+        <p>{{ slotProps['anoth'] }}</p>
+
+    </course-goals>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue'
-import BadgeList from './components/BadgeList.vue'
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+import BadgeList from "./components/BadgeList.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   components: {
     TheHeader,
     UserInfo,
-    BadgeList
+    BadgeList,
+    CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
---
+++ section09_deep_cmp/1_cmp_adv/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" anoth="박수영"></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['뷰를 끝까지 완료하자','vue학습']
+        }
+    },
+}
+</script>
