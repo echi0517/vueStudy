c62c6f508afc44dade66d1c443cfa2c179603ba5
DESKTOP-NPLM7LE\cube <sypark>
2023-03-30 18:09:03 +0900

    유용한사이트 등록 프로젝트 공부

---
+++ section10_lean_resource_pjt/1_learn_resource_pjt/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <base-card>
+      <base-button
+        @click="setSelectedTab('stored-resources')"
+        :mode="storedResButtonMode"
+        >Stored Resources</base-button
+      >
+      <base-button
+        @click="setSelectedTab('add-resource')"
+        :mode="addResButtonMode"
+        >Add Resource</base-button
+      >
+    </base-card>
+    <keep-alive>
+      <component :is="seletedTab"></component>
+    </keep-alive>
+  </div>
+</template>
+<script>
+import StoredResources from "./StoredResources.vue";
+import AddResource from "./AddResource.vue";
+export default {
+  components: { StoredResources, AddResource },
+  computed: {
+    storedResButtonMode() {
+      return this.seletedTab === "stored-resources" ? null : "flat";
+    },
+    addResButtonMode() {
+      return this.seletedTab === "add-resource" ? null : "flat";
+    },
+  },
+  data() {
+    return {
+      seletedTab: "stored-resources",
+      storedResources: [
+        {
+          id: "offce-guide",
+          title: "Officeal Guide",
+          description: "vuejs documentation",
+          link: "https://vuejs.org/",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "google",
+          link: "https://google.com/",
+        },
+      ],
+    };
+  },
+
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource,
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.seletedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.seletedTab = "stored-resources";
+    },
+  },
+};
+</script>
